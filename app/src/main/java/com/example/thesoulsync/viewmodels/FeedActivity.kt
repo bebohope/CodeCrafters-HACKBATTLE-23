e92928514cc85b5be9569314dd1dbb9dd549aea9
Harsh Raj A <Hraj20000@gmail.com>
2023-09-20 03:31:09 +0530

    added activityfeed

---
+++ app/src/main/java/com/example/thesoulsync/viewmodels/FeedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.thesoulsync.viewmodels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.thesoulsync.R
+
+class FeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+    }
+}
